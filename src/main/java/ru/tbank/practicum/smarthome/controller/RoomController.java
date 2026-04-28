3f257bac10716b166137ece063c98a4b0471dff9
Egor Efimov <efimoved18@mail.ru>
2026-04-29 00:54:26 +0400

    изменил миграции,добавил возможность создания комнат пользователем, пофиксил gitignore

---
+++ src/main/java/ru/tbank/practicum/smarthome/controller/RoomController.java
@@ -0,0 +1,4 @@
+package ru.tbank.practicum.smarthome.controller;
+
+public class RoomController {
+}
---
+++ src/main/java/ru/tbank/practicum/smarthome/controller/dto/RoomDto.java
@@ -0,0 +1,4 @@
+package ru.tbank.practicum.smarthome.controller.dto;
+
+public class RoomDto {
+}
